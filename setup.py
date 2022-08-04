6af6b9f4b77336039246f96f46da87c932eab295
Andy Kipp <anki-code@users.noreply.github.com>
2022-08-04 14:58:19 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except (IOError, OSError):
 
 setuptools.setup(
     name='xontrib-cheatsheet',
-    version='0.1.0',
+    version='0.2.0',
     license='MIT',
     author='anki-code',
     author_email='no@no.no',
