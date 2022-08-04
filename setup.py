e1b364892ab67fbd6ac320f36e1d298d7c6164c9
Andy Kipp <anki-code@users.noreply.github.com>
2022-08-04 14:58:34 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,10 +30,6 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: System :: Shells",
